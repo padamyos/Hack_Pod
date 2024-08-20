1421eac19bbd8bcaf2d5b18816deee09f2376137
padamyos <padamyos.p@rmutsvmail.com>
2024-08-20 10:37:06 +0700

    setup taillwind

---
+++ smoke_web/src/App.vue
@@ -0,0 +1,14 @@
+<script setup >
+import {ref} from 'vue';
+import Navbar from './components/Navbar.vue';
+// import Login from './view/Login.vue';
+import SignUp from './view/SignUp.vue';
+</script>
+
+<template>
+    <div>
+        <Navbar />
+         <Login />
+        <SignUp /> 
+    </div>
+</template>
