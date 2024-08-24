81b562c142a40ba0ed16039f2e5d67c0e379c4d0
padamyos <padamyos.p@rmutsvmail.com>
2024-08-24 09:25:08 +0700

    add router

--- smoke_web/src/App.vue
+++ smoke_web/src/App.vue
@@ -1,13 +1,20 @@
-<script setup >
-import {ref} from 'vue';
+<script setup>
+import { ref } from 'vue';
 import Navbar from './components/Navbar.vue';
 import Login from './view/Login.vue';
 // import SignUp from './view/SignUp.vue';
+import Footer from './components/Footer.vue'; // Import Footer component
 </script>
 
 <template>
     <div>
         <Navbar />
-        <Login />
+
+        <main>
+            <RouterView />
+        </main>
+
+        <Footer />
+
     </div>
 </template>
